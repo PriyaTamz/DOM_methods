ae0132f579145c66978ffd5339679f989c51aa5e
Vishnupriya1996TH <137433754+Vishnupriya1996TH@users.noreply.github.com>
2024-07-15 12:05:47 +0530

    Add files via upload
    Create an HTML form using the DOM method

---
+++ script.js
@@ -0,0 +1,16 @@
+function foo() {
+    var firstName = document.getElementById("firstname").value;
+    console.log("First Name:", firstName);
+
+    var lastName = document.getElementById("lastname").value;
+    console.log("Last Name:", lastName);
+
+    var middleName = document.getElementById("middlename").value;
+    console.log("Middle Name:", middleName);
+
+    var email = document.getElementById("email").value;
+    console.log("Email:", email);
+
+    var password = document.getElementById("password").value;
+    console.log("Password:", password);
+}
